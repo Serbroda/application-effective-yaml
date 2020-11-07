b5d3d4729df86b4ba98327a2c9afe53c52e7ceae
Danny Rottstegge <danny@rottstegge.net>
2020-11-07 23:09:12 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='application-effective-yaml',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/Serbroda/application-effective-yaml',
+    license='Apache 2.0',
+    author='Danny Rottstegge',
+    author_email='',
+    description='Application effective yaml merger'
+)
