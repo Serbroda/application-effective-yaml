123164a5e62902c817d052a08e1728616ca71c33
Danny Rottstegge <d.rottstegge@deutsche-glasfaser.de>
2020-11-09 13:58:01 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-mkdir ./target
-python3 -m zipapp app -m "main:main" -o ./target/application_effective_yaml.pyz
+rm -rf ./target
+mkdir -p ./target
+mkdir -p ./target/build
+
+python3 -m pip install -r requirements.txt --target ./target/build
+cp -a ./app/. ./target/build/
+
+python3 -m zipapp target/build -o ./target/application_effective_yaml.pyz
